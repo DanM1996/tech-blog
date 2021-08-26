dc41c7b8f1b8270c1f937993e1b5c63b7f4da07d
Dan <bmdmartinez@gmail.com>
2021-08-26 11:16:04 -0400

    created handlebar routes. resolving SequelizeConnectionRefusedError

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-// const helpers = require('./utils/helper');
+const helpers = require('./utils/helper');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,10 +19,10 @@ const sess = {
     })
 };
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
